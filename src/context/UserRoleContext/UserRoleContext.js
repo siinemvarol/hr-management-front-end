524706e7f8df43f324d64c33b888720cfca763b1
siinemvarol <siinemvarol@gmail.com>
2023-09-28 17:27:47 +0300

    task209_sidenav_refresh

--- src/context/UserRoleContext/UserRoleContext.js
+++ src/context/UserRoleContext/UserRoleContext.js
@@ -1,10 +1,31 @@
 import React, { createContext, useContext, useState } from "react";
 import PropTypes from "prop-types"; // Import PropTypes
+import jwt_decode from "jwt-decode";
+import { useEffect } from "react";
 
 const UserRoleContext = createContext();
 
 export const UserRoleProvider = ({ children }) => {
-  const [userRole, setUserRole] = useState("all"); // Initial user role
+  const [userRole, setUserRole] = useState(() => {
+    // Initialize userRole from localStorage or default to "all" roles
+    const storedToken = localStorage.getItem("Authorization");
+    const storedUserRole = jwt_decode(storedToken).role;
+    return storedUserRole || "all";
+  });
+
+  useEffect(() => {
+    // Check if there is a token in localStorage
+    const storedToken = localStorage.getItem("Authorization");
+    if (storedToken) {
+      // If a token is present, decode it and set the user's role
+      const decodedToken = jwt_decode(storedToken);
+      const decodedUserRole = decodedToken.role;
+      setUserRole(decodedUserRole);
+
+      // Store the user role in localStorage
+      localStorage.setItem("userRole", decodedUserRole);
+    }
+  }, []); // Only run this effect once during component initialization
 
   return (
     <UserRoleContext.Provider value={{ userRole, setUserRole }}>
@@ -14,9 +35,8 @@ export const UserRoleProvider = ({ children }) => {
 };
 
 UserRoleProvider.propTypes = {
-  children: PropTypes.node.isRequired, // Use PropTypes.node to validate that 'children' is a valid React node
+  children: PropTypes.node.isRequired,
 };
-
 
 export const useCentralState = () => {
   const context = useContext(UserRoleContext);
